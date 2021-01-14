47e4818e9befba04fdd8b8eca2156de1cddcbb4b
Abdul Hafidz Anshari <aditans88@gmail.com>
2021-01-14 14:10:06 +0700

    remove tracer zipper

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Routing\RouteFactory;
-use App\Routing\RouteRegistry;
-use App\Services\RestClientFactory;
-use App\Services\RestClientInterface;
-
 return [
-    RouteRegistry::class => RouteFactory::class,
+    // 
 ];
